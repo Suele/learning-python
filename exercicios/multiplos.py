f563582cb8ff2d9181cee4158348c5e1a1b830fb
suele <suelen.ads.guimaraes@gmail.com>
2020-11-04 23:56:23 -0300

    Adiciona o exercicio de numeros multiplos.

---
+++ exercicios/multiplos.py
@@ -0,0 +1,21 @@
+print('Digite um numero inteiro.')
+number = int(input())
+result = 0
+print()
+
+if (number % 3 == 0):
+    print(f'Multiplo de 3')
+if (number % 5 == 0):
+    print(f'multiplo de 5')
+if (number % 4 == 0):
+    print('multiplo de 4')
+if (number % 2 == 0):
+    print('multiplo de 2')
+if (number % 6 == 0):
+    print('multiplo de 6')
+if (number % 7 == 0):
+    print('multiplo de 7')
+if (number % 8 == 0):
+    print('multiplo de 8')
+if (number % 9 == 0):
+    print('multiplo de 9')
