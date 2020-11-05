249b35e60687f209479205a74d2817d0fede3f7f
suele <suelen.ads.guimaraes@gmail.com>
2020-11-04 23:55:16 -0300

    Adiciona conceitos de listas.

---
+++ exercicios/Listas/conceitosDeListas.py
@@ -0,0 +1,6 @@
+"""
+-Em python listas são os arrays em outras linguagens como em java.
+-Em python as listas não possuem valor fixo e podem possuir qualquer tipo de dados, por exemplo posso ter uma lista com strings e ints juntos.
+-As listas em python são representadas por [].
+
+"""
